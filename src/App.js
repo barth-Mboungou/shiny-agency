880d12eb18bea8b111cf7f0e7a43bf38f36da8b2
barth-Mboungou <barthmboungou@gmail.com>
2024-11-23 15:05:31 +0100

    Créez une route pour les attraper toutes : 404

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import './App.css';
-import Header from './components/header';
-import Home from './pages/home/Home';
-import Survey from './pages/survey/survey';
-import ClientForm from './components/ClientForm';
-import FreelanceForm from './components/FreelanceForm';
+import './App.css'
+import Header from './components/header'
+import Home from './pages/home/Home'
+import Survey from './pages/survey/survey'
+import ClientForm from './components/ClientForm'
+import FreelanceForm from './components/FreelanceForm'
+import Error from './components/Error'
 
 function App() {
   return (
     <>
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/survey/:questionNumber" element={<Survey />} />
-        { /* Nous imbriquons nos composants dans survey */}
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/survey/:questionNumber" element={<Survey />} />
+          {/* Nous imbriquons nos composants dans survey */}
           <Route path="client" element={<ClientForm />} />
           <Route path="freelance" element={<FreelanceForm />} />
-      </Routes>
-     </Router>
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
     </>
   )
 }
-export default App;
+export default App
